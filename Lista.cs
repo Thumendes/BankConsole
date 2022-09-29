d3fe761f021684a4fbbb7a7c8187043e0e724f58
Arthur Mendes <thumendess@gmail.com>
2022-09-29 09:14:37 -0300

    Initial

---
+++ Lista.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+
+namespace ListaTeste
+{
+    public class Node<T>
+    {
+        public T value;
+        public Node<T>? next;
+
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+
+    public class Lista<T>
+    {
+        public Node<T>? head;
+
+        public Lista<T> Add(T value)
+        {
+            var node = new Node<T>(value);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node<T> aux = head;
+                while (aux.next != null)
+                {
+                    aux = aux.next;
+                }
+
+                aux.next = node;
+            }
+
+            return this;
+        }
+
+
+        public delegate void ForEachCallback(T value, int index);
+        public void ForEach(ForEachCallback Callback)
+        {
+            Node<T>? aux = head;
+            int index = 0;
+            while (aux != null)
+            {
+                Callback(aux.value, index);
+                aux = aux.next;
+                index++;
+            }
+        }
+
+        public delegate V MapCallback<V>(T value, int index);
+        public Lista<V> Map<V>(MapCallback<V> Callback)
+        {
+            var newList = new Lista<V>();
+            ForEach((value, index) => newList.Add(Callback(value, index)));
+            return newList;
+        }
+
+        public delegate bool FilterCallback(T value, int index);
+        public Lista<T> Filter(FilterCallback Callback)
+        {
+            var newList = new Lista<T>();
+            ForEach((value, index) =>
+            {
+                if (Callback(value, index)) newList.Add(value);
+            });
+            return newList;
+        }
+
+        public delegate bool FindCallback(T value, int index);
+        public T? Find(FindCallback Callback)
+        {
+            Node<T>? aux = head;
+            int index = 0;
+            while (aux != null)
+            {
+                if (Callback(aux.value, index)) return aux.value;
+                aux = aux.next;
+                index++;
+            }
+            return default(T);
+        }
+
+        public delegate bool FindIndexCallback(T value, int index);
+        public int FindIndex(FindIndexCallback Callback)
+        {
+            Node<T>? aux = head;
+            int index = 0;
+            while (aux != null)
+            {
+                if (Callback(aux.value, index)) return index;
+                aux = aux.next;
+                index++;
+            }
+            return -1;
+        }
+
+        public delegate V ReduceCallback<V>(V acc, T value, int index);
+        public V Reduce<V>(ReduceCallback<V> Callback, V value)
+        {
+            Node<T>? aux = head;
+            int index = 0;
+            while (aux != null)
+            {
+                value = Callback(value, aux.value, index);
+                aux = aux.next;
+                index++;
+            }
+            return value;
+        }
+
+        public T? At(int index)
+        {
+            Node<T>? aux = head;
+            int currIndex = 0;
+            while (aux != null)
+            {
+                if (currIndex == index) return aux.value;
+                aux = aux.next;
+                currIndex++;
+            }
+            return default(T);
+        }
+
+        public void Remove(T value)
+        {
+            Node<T>? aux = head;
+            Node<T>? prev = head;
+            while (aux != null && aux.value != null && prev != null)
+            {
+                if (aux.value.Equals(value)) prev.next = aux.next;
+                prev = aux;
+                aux = aux.next;
+            }
+        }
+
+        public int Length()
+        {
+            var count = 0;
+            ForEach((value, index) => count++);
+            return count;
+        }
+
+        public override string ToString()
+        {
+            string text = "{";
+            int size = Length();
+            ForEach((value, index) =>
+            {
+                string additional = index == size - 1 ? $"{value}}}" : $"{value}, ";
+                text += additional;
+            });
+            return text;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using ListaTeste;
+using Spectre.Console;
+
+namespace ListaTeste
+{
+    public enum EventsCode
+    {
+        Add,
+        Remove
+    }
+
+    public delegate void MenuAction();
+    public record MenuItem(ConsoleKey Code, string Text, MenuAction Action);
+    public record Event<T>(EventsCode type, T value);
+
+    class Bank
+    {
+        public bool running;
+        private double amount;
+        private readonly Lista<MenuItem> menuList;
+        private readonly Lista<Event<double>> eventsList;
+
+        public Bank()
+        {
+            menuList = new Lista<MenuItem>()
+                .Add(new MenuItem(ConsoleKey.D1, "Adicionar dinheiro", AddMoney))
+                .Add(new MenuItem(ConsoleKey.D2, "Remover dinheiro", RemoveMoney))
+                .Add(new MenuItem(ConsoleKey.D3, "Mostrar histórico", ShowHistory))
+                .Add(new MenuItem(ConsoleKey.D4, "Sair", Finish));
+
+            eventsList = new Lista<Event<double>>();
+        }
+
+        public void Start()
+        {
+            running = true;
+
+            while (running)
+            {
+                Console.Clear();
+                ShowMenu();
+                AnsiConsole.Markup("Digite o item do menu: ");
+
+                var key = Console.ReadKey();
+                var menuItem = menuList.Find((value, index) => value.Code.Equals(key.Key));
+                if (menuItem != null) menuItem.Action();
+                else Console.WriteLine();
+            }
+        }
+
+        private void ShowMenu()
+        {
+            var textMenu = menuList.Reduce((acc, value, index) => acc += $"[bold gray]{value.Code})[/] {value.Text}\n", "");
+            AnsiConsole.Markup(textMenu);
+            var color = amount < 0 ? "red" : amount > 0 ? "green" : "blue";
+            AnsiConsole.Markup($"Seu saldo é [bold {color}]${amount}[/].\n");
+        }
+
+        private void AddMoney()
+        {
+            Console.Clear();
+            var addAmount = AnsiConsole.Ask<double>("Digite o valor: ");
+            amount += addAmount;
+            eventsList.Add(new Event<double>(EventsCode.Add, addAmount));
+        }
+
+        private void RemoveMoney()
+        {
+            Console.Clear();
+            var removeAmount = AnsiConsole.Ask<double>("Digite o valor: ");
+            amount -= removeAmount;
+            eventsList.Add(new Event<double>(EventsCode.Remove, removeAmount));
+        }
+
+        private void ShowHistory()
+        {
+            var text = eventsList.Reduce((acc, @event, index) =>
+            {
+                if (@event.type.Equals(EventsCode.Add))
+                {
+                    acc += $"Adicionou valor de ${@event.value}";
+                }
+
+                if (@event.type.Equals(EventsCode.Remove))
+                {
+                    acc += $"Removeu valor de ${@event.value}";
+                }
+
+                return acc += "\n";
+            }, "");
+            text += "[gray]Aperte qualquer coisa para voltar...[/]";
+
+            Console.Clear();
+            AnsiConsole.Markup(text);
+            Console.ReadKey();
+        }
+
+        private void Finish()
+        {
+            Console.Clear();
+            AnsiConsole.Markup("[bold]Até a próxima![/]");
+            running = false;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var program = new Bank();
+            program.Start();
+            Console.ReadKey();
+        }
+    }
+}
